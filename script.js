8731e0b005b21cd4d5b1e8dbf638cf2b7c8d5c8c
uhcats <stanislawplust@gmail.com>
2022-07-19 21:11:30 +0200

    End script in script.js

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 const lengthNumber = document.getElementById('lengthNumber');
 const containNumber = document.getElementById('containNumber');
 const containSymbols = document.getElementById('containSymbols');
+const text = document.getElementById('text');
+const btn = document.getElementById('btn');
 
-
-const textValue = text.value;
-let result = " ";
-const upperLetter = "QWERTYUIOPASDFGHJKLZXCVBNM";
+let result = "";
 const downLetter = "qwertyuiopasdfghjklzxcvbnm";
 const number = "0123456789";
 const symbols = '!@#$%^&*()_-=+';
 
-let passwordLength = lengthNumber.value;
+
 
 const SymbolsDownLetter = downLetter.concat(symbols);
 const NumberDownLetter = downLetter.concat(number);
 const SymbolsAndNumber = SymbolsDownLetter.concat(NumberDownLetter);
 
+btn.addEventListener('click', () => {
+  text.innerText = "";
+  result = "";
+  return checkChar()
+
+})
+
 
 function useTemplate(letter) {
-  for (let i = 0; i < passwordLength; i++) {
+  let passwordLength = lengthNumber.value;
+  for (let i = 0; i < passwordLength; i++)
     result += letter.charAt(Math.floor(Math.random() * letter.length));
-  }
-  console.log(result);
+  text.innerText = result;
 }
 
-if (containNumber.checked && containSymbols.checked === true) {
-  useTemplate(SymbolsAndNumber);
-} else if (containSymbols.checked) {
-  useTemplate(SymbolsDownLetter);
-} else if (containNumber.checked) {
-  useTemplate(NumberDownLetter);
+function checkChar() {
+  if (containNumber.checked && containSymbols.checked)
+    useTemplate(SymbolsAndNumber);
+  else if (containSymbols.checked)
+    useTemplate(SymbolsDownLetter);
+  else if (containNumber.checked)
+    useTemplate(NumberDownLetter);
+  else
+    useTemplate(downLetter);
 }
